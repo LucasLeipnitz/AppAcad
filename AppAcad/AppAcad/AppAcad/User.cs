ea61cac25e3bc476ddd637fe213aaad35fa0dd13
Lucas <lucas.leipnitz.f@hotmail.com>
2020-07-01 20:50:19 -0300

    Criação da classe User

---
+++ AppAcad/AppAcad/AppAcad/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppAcad
+{
+    class User
+    {
+        private string name;
+        private float height;
+        private float weight;
+        private float waist;
+
+        public User(string name, float height, float weight, float waist)
+        {
+            this.name = name;
+            this.height = height;
+            this.weight = weight;
+            this.waist = waist;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public float getHeight()
+        {
+            return height;
+        }
+
+        public float getWeight()
+        {
+            return weight;
+        }
+
+        public float getWaist()
+        {
+            return waist;
+        }
+    }
+}
